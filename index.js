69997e7a5ea6414101ed2a502e06d0ceba06a788
LDHugeman <luisdavid.vp@gmail.com>
2021-09-12 06:20:29 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT,() => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
+
+app.get('/', (request, response) => {
+  response.send('Hello world')
+})
+
+ 
